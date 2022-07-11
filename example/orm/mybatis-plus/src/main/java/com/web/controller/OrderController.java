72e485be195d65222ea4ecbf2958b7c6e706a751
mac <18810027915@163.com>
2022-07-11 22:28:07 +0800

    8008

--- example/orm/mybatis-plus/src/main/java/com/web/controller/OrderController.java
+++ example/orm/mybatis-plus/src/main/java/com/web/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.web.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.web.entity.OrderDO;
 import com.web.service.OrderService;
 import com.web.vo.common.PageQueryVO;
 import com.web.vo.common.ResponseVO;
@@ -11,6 +13,8 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -29,88 +33,53 @@ public class OrderController {
 
     @PostMapping("save")
     @ApiOperation(value = "批量增加订单")
-    public ResponseVO save(OrderBatchVO orderBatchVO) {
-        try {
-
-            for (int i = orderBatchVO.getBegin();i < orderBatchVO.getEnd(); i++) {
-                final int j = i;
-                log.info("提交任务: " + j);
-                poolExecutor.submit(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            orderService.save(j);
-                        } catch (Exception e) {
-                            log.error("错误: ", e);
-                        }
+    public void save(OrderBatchVO orderBatchVO) {
+        for (int i = orderBatchVO.getBegin();i < orderBatchVO.getEnd(); i++) {
+            final int j = i;
+            log.info("提交任务: " + j);
+            poolExecutor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        orderService.save(j);
+                    } catch (Exception e) {
+                        log.error("错误: ", e);
                     }
-                });
-            }
-            return ResponseVO.SUCCESS("ok");
-        } catch (Exception e) {
-            log.error("method:save 异常", e);
-            return ResponseVO.FAIL(e.getMessage());
+                }
+            });
         }
     }
 
     @PutMapping("updateStatus")
     @ApiOperation(value = "更新状态, 事务测试")
-    public ResponseVO updateStatus(@RequestBody OrderUpdateStatusVO statusVO) {
-        try {
-            Integer a = Integer.valueOf(statusVO.getStatus());
-            log.info("修改后状态: " + a + " =============== ");
-            orderService.update(statusVO);
-            return ResponseVO.SUCCESS(null);
-        } catch (Exception e) {
-            log.error("method:findOne 异常", e);
-            return ResponseVO.FAIL(e.getMessage());
-        }
+    public void updateStatus(@RequestBody OrderUpdateStatusVO statusVO) {
+        Integer a = Integer.valueOf(statusVO.getStatus());
+        log.info("修改后状态: " + a + " =============== ");
+        orderService.update(statusVO);
     }
 
     @GetMapping("findByOrderNo/{orderNo}")
     @ApiOperation(value = "根据订单号查询")
-    public ResponseVO findByOrderNo(@PathVariable("orderNo") String orderNo) {
-        try {
-            return ResponseVO.SUCCESS(orderService.findByOrderNo(orderNo));
-        } catch (Exception e) {
-            log.error("method:findOne 异常", e);
-            return ResponseVO.FAIL(e.getMessage());
-        }
+    public OrderDO findByOrderNo(@PathVariable("orderNo") String orderNo) {
+        return orderService.findByOrderNo(orderNo);
     }
 
     @GetMapping("findByTakeGoodsCode/{takeGoodsCode}")
     @ApiOperation(value = "根据取件码查询")
-    public ResponseVO findByTakeGoodsCode(@PathVariable("takeGoodsCode") String takeGoodsCode) {
-        try {
-            return ResponseVO.SUCCESS(orderService.findByTakeGoodsCode(takeGoodsCode));
-        } catch (Exception e) {
-            log.error("method:findOne 异常", e);
-            return ResponseVO.FAIL(e.getMessage());
-        }
+    public OrderDO findByTakeGoodsCode(@PathVariable("takeGoodsCode") String takeGoodsCode) {
+        return orderService.findByTakeGoodsCode(takeGoodsCode);
     }
 
     @PostMapping("findByDeliveryCompanyName")
     @ApiOperation(value = "根据运力名称查询")
-    public ResponseVO findByDeliveryCompanyName(@RequestBody FindByDeliveryCompanyNameReqVO deliveryCompanyNameReqVO) {
-        try {
-            return ResponseVO.SUCCESS(
-                    orderService.findByDeliveryCompanyName(deliveryCompanyNameReqVO.getDeliveryCompanyName()));
-        } catch (Exception e) {
-            log.error("method:findOne 异常", e);
-            return ResponseVO.FAIL(e.getMessage());
-        }
+    public List<OrderDO> findByDeliveryCompanyName(@RequestBody FindByDeliveryCompanyNameReqVO deliveryCompanyNameReqVO) {
+        return orderService.findByDeliveryCompanyName(deliveryCompanyNameReqVO.getDeliveryCompanyName());
     }
 
     @PostMapping("findByPage")
     @ApiOperation(value = "分页查询")
-    public ResponseVO findByPage(PageQueryVO pageQueryVO) {
-        try {
-            return ResponseVO.SUCCESS(
-                    orderService.findByPage(pageQueryVO));
-        } catch (Exception e) {
-            log.error("method:findByPage 异常", e);
-            return ResponseVO.FAIL(e.getMessage());
-        }
+    public IPage<OrderDO> findByPage(PageQueryVO pageQueryVO) {
+            return orderService.findByPage(pageQueryVO);
     }
 
 }
