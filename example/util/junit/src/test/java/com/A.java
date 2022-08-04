9f7af1daa195170a1e51286577e2616592aa6ffc
wuheping <wHp122066323>
2022-08-04 21:35:49 +0800

    8008

---
+++ example/util/junit/src/test/java/com/A.java
@@ -0,0 +1,18 @@
+package com;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = JunitApplication.class)
+public class A {
+
+    @Test
+    public void aa() {
+        log.info("============");
+    }
+}
