0d3753602c432f8fb4af946b5b6f27c9eb9e45ad
wuheping <wHp122066323>
2022-10-11 21:43:13 +0800

    8008

--- example/text-graph/aspose-doc/src/main/java/com/AsposeDocApplication.java
+++ example/text-graph/aspose-doc/src/main/java/com/AsposeDocApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AsposeApplication {
+public class AsposeDocApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AsposeApplication.class, args);
+        SpringApplication.run(AsposeDocApplication.class, args);
     }
 }
