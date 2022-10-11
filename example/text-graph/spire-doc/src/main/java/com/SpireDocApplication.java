e743dcfad3ba53c73344a351bc0638ef24c76e48
wuheping <wHp122066323>
2022-10-11 22:49:47 +0800

    8008

---
+++ example/text-graph/spire-doc/src/main/java/com/SpireDocApplication.java
@@ -0,0 +1,11 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpireDocApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpireDocApplication.class, args);
+    }
+}
