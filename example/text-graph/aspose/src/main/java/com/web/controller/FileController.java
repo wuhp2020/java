8532fa9671608c975a14889f0676583c5515a2d4
wuheping <wHp122066323>
2022-10-11 21:33:54 +0800

    8008

--- example/text-graph/aspose/src/main/java/com/web/controller/FileController.java
+++ example/text-graph/aspose/src/main/java/com/web/controller/FileController.java
@@ -1,17 +1,18 @@
 package com.web.controller;
 
-import com.web.util.Office2PDFUtil;
+import com.aspose.words.Document;
+import com.aspose.words.SaveFormat;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 @RestController
 @RequestMapping("/api/v1/file")
@@ -22,25 +23,19 @@ public class FileController {
     @GetMapping("/view")
     @ApiOperation(value = "预览")
     public void view(HttpServletResponse response) throws Exception {
-        String netFileUrl = "https://study-dfs.sugon.com/group1/M00/00/09/CgJoA2ND5iuASj5AAJ9K7L435CQ85.pptx";
-        URL url = new URL(netFileUrl);
-        HttpURLConnection httpUrl = (HttpURLConnection) url.openConnection();
-        httpUrl.connect();
-
+        // 输出格式
         response.reset();
         response.setContentType("application/pdf");
         response.setHeader("Content-Disposition", "inline; filename=a.pdf");
 
-        InputStream inputStream = httpUrl.getInputStream();
+        // 获取本地文件或网络文件
+        Resource resource = new ClassPathResource("1.docx");
+        InputStream inputStream = resource.getInputStream();
         OutputStream outputStream = response.getOutputStream();
-        if (netFileUrl.endsWith(".docx") || netFileUrl.endsWith(".doc")) {
-            Office2PDFUtil.doc2pdf(inputStream, outputStream);
-        } else if (netFileUrl.endsWith(".xlsx") || netFileUrl.endsWith(".xls")) {
-            Office2PDFUtil.excel2pdf(inputStream, outputStream);
-        } else if (netFileUrl.endsWith(".pptx") || netFileUrl.endsWith(".ppt")) {
-            Office2PDFUtil.ppt2pdf(inputStream, outputStream);
-        }
-        httpUrl.disconnect();
+
+        Document doc = new Document(inputStream);
+        doc.save(outputStream, SaveFormat.PDF);
+
         inputStream.close();
         outputStream.close();
     }
