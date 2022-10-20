0493e9b63d69340f0029bf755e1b7bb9db55614e
wuheping <wHp122066323>
2022-10-20 22:59:18 +0800

    8008

--- example/low-code/magic-api/src/main/java/com/MagicApiApplication.java
+++ example/low-code/magic-api/src/main/java/com/MagicApiApplication.java
@@ -2,10 +2,14 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.ssssssss.magicapi.core.config.MagicConfiguration;
+import org.ssssssss.magicapi.core.model.ApiInfo;
 
 @SpringBootApplication
 public class MagicApiApplication {
     public static void main(String[] args) {
+//        ApiInfo apiInfo = new ApiInfo();
+//        MagicConfiguration.getMagicResourceService().saveFile(apiInfo);
         SpringApplication.run(MagicApiApplication.class, args);
     }
 }
