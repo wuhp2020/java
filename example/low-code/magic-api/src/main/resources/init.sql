1aa3c56b228c63cfbba00211b84082941f8b801b
wuheping <18810027915@163.com>
2023-01-06 10:06:23 +0800

    8008

--- example/low-code/magic-api/src/main/resources/init.sql
+++ example/low-code/magic-api/src/main/resources/init.sql
@@ -5,21 +5,15 @@ CREATE TABLE `magic_api`  (
   PRIMARY KEY (`id`) USING BTREE
 );
 
-CREATE TABLE `department` (
-  `id` bigint(20) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
 CREATE TABLE `employee` (
-  `id` bigint(20) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `age` bigint(20) NOT NULL,
-  `departmentid` varchar(255) DEFAULT NULL,
-  `intotime` date DEFAULT NULL,
-  `salary` double DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `employee_age_name` (`age`,`name`),
-  KEY `employee_salary_name` (`salary`,`name`),
-  KEY `employee_departid` (`departmentid`)
+    `id` bigint(20) NOT NULL COMMENT '主键',
+    `name` varchar(255) DEFAULT NULL COMMENT '姓名',
+    `age` bigint(20) NOT NULL COMMENT '年龄',
+    `departmentid` varchar(255) DEFAULT NULL COMMENT '部门id',
+    `intotime` date DEFAULT NULL COMMENT '日期',
+    `salary` double DEFAULT NULL COMMENT '薪资',
+    PRIMARY KEY (`id`),
+    KEY `employee_age_name` (`age`,`name`),
+    KEY `employee_salary_name` (`salary`,`name`),
+    KEY `employee_departid` (`departmentid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
